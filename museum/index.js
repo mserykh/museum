d9de6b745ef3b6079802d51be77d840f09530427
Marie Serykh <marie.serykh@gmail.com>
2021-09-27 22:02:42 +0200

    refactor: adjust progress bar background-color of video player

--- museum/index.js
+++ museum/index.js
@@ -2,14 +2,14 @@ const progressVideo = document.querySelector('.controls__progress-bar');
   
 progressVideo.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%)`
+  this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, #C4C4C4 ${value}%)`
 })
  
 const volumeChange = document.querySelector('.controls__volume-slider');
   
 volumeChange.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%)`
+  this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, #C4C4C4 ${value}%)`
 })
  
 
