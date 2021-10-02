05414e7a0c7f59033aa9cf21d5bdf0693dba6048
Marie Serykh <marie.serykh@gmail.com>
2021-10-02 18:29:42 +0200

    refactor: place form scripts in separate file

--- museum-adaptive/index.js
+++ museum-adaptive/index.js
@@ -12,49 +12,6 @@ volumeChange.addEventListener('input', function() {
   this.style.background = `linear-gradient(to right, var(--color-accent2) 0%, var(--color-accent2) ${value}%, var(--color-player2) ${value}%)`
 })
 
-const bookBtn = document.querySelector('.order-form__btn-buy');
-const closeBookingBtn = document.querySelector('.booking__btn-close');
-const modalWindow = document.querySelector('.booking');
-const overlay = document.querySelector('.overlay');
-
-function showBookingForm() {
-  modalWindow.classList.add('open');
-  overlay.classList.add('open');
-}
-
-function closeBookingForm() {
-  modalWindow.classList.remove('open');
-  overlay.classList.remove('open');
-}
-
-bookBtn.addEventListener('click', showBookingForm);
-closeBookingBtn.addEventListener('click', closeBookingForm);
-
-
-const paymentBtn = document.querySelector('.booking-payment__btn');
-
-
-function addRipple(event) {
-  const circle = document.createElement('div');
-  const circleDiameter = Math.max(paymentBtn.clientWidth, paymentBtn.clientHeight);
-  const crcl = paymentBtn.getBoundingClientRect();
-  const circleRadius = circleDiameter / 2;
-  circle.style.width = `${circleDiameter}px`;
-  circle.style.height = `${circleDiameter}px`;
-  circle.style.top = `${event.clientY - crcl.top - circleRadius}px`;
-  circle.style.left = `${event.clientX - crcl.left - circleRadius}px`;
-  circle.classList.add('ripple');
-  const ripple = paymentBtn.getElementsByClassName('ripple')[0];
-
-  if (ripple) {
-    ripple.remove();
-  }
-
-  paymentBtn.appendChild(circle);
-}
-
-paymentBtn.addEventListener('click', addRipple);
-
 const toggleBtn = document.querySelector('.menu-toggle');
 const navHeader = document.querySelector('.header__navigation');
 
