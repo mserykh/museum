2dc4f86d8252c8a23da7a84cb55a7a3f08b5860b
Marie Serykh <marie.serykh@gmail.com>
2021-10-06 15:05:13 +0200

    feat: implement opening and closing mobile window

--- museum-adaptive/index.js
+++ museum-adaptive/index.js
@@ -11,3 +11,22 @@ volumeChange.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, var(--color-accent2) 0%, var(--color-accent2) ${value}%, var(--color-player2) ${value}%)`
 })
+
+const toggleBtn = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-navigation');
+
+const welcomeContent = document.querySelector('.welcome__content-wrapper');
+
+function toggleMenu() {
+  toggleBtn.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-active');
+ 
+  hideContent();
+}
+
+function hideContent(event) {
+  console.log(event);
+  welcomeContent.classList.toggle('is-hidden');
+}
+
+toggleBtn.addEventListener('click', toggleMenu);
