578c917eff394c6158103979b13479d468848e5b
Marie Serykh <marie.serykh@gmail.com>
2021-10-09 17:32:05 +0200

    feat: add validation of name, email and phone umber for modal window

--- museum-dom/form.js
+++ museum-dom/form.js
@@ -1,7 +1,26 @@
+const FORM = document.getElementById('booking-info')
 const bookBtn = document.querySelector('.order-form__btn-buy');
 const closeBookingBtn = document.querySelector('.booking__btn-close');
 const modalWindow = document.querySelector('.booking');
 const overlay = document.querySelector('.overlay');
+const paymentBtn = document.querySelector('.booking-payment__btn');
+const INPUT_DATE = document.querySelector('.input--date');
+const INPUT_TIME = document.querySelector('.input--time');
+const INPUT_NAME = document.querySelector('.input--name');
+const INPUT_EMAIL = document.querySelector('.input--email');
+const INPUT_PHONE = document.querySelector('.input--phone');
+const SELECT_TICKET_TYPE = document.querySelector('.field__ticket-type');
+const TICKET_DATE = document.querySelector('.overview__date');
+
+const TODAY = new Date();
+const MIN_DATE = TODAY.toISOString().slice(0, 10);
+
+const ERROR_TEXT_NAME = 'Please enter your first name, e.g. Alexandra I. Please use 3 to 15 latin or cyrilic characters and spaces.';
+const ERROR_TEXT_EMAIL = 'Please enter a valid email, e.g. User-Name_@exapmle.com.';
+const ERROR_TEXT_PHONE = 'Please enter a valid phone number, maximum 10 digits, eg. 628-456-179';
+const ERROR_TEXT_DATE = 'Please pick a date of your visit.';
+const ERROR_TEXT_TIME = 'Please pick a time of your visit.';
+const ERROR_TEXT_TICKET_TYPE = 'Please select a ticket type.'
 
 function showBookingForm() {
   modalWindow.classList.add('open');
@@ -12,13 +31,6 @@ function closeBookingForm() {
   modalWindow.classList.remove('open');
   overlay.classList.remove('open');
 }
-
-bookBtn.addEventListener('click', showBookingForm);
-closeBookingBtn.addEventListener('click', closeBookingForm);
-
-
-const paymentBtn = document.querySelector('.booking-payment__btn');
-
 
 function addRipple(event) {
   const circle = document.createElement('div');
@@ -39,16 +51,171 @@ function addRipple(event) {
   paymentBtn.appendChild(circle);
 }
 
-paymentBtn.addEventListener('click', addRipple);
-
-
-const inputDate = document.querySelector('.input--date');
-const inputTime = document.querySelector('.input--time');
-
 function getValue() {
   let value = this.value;
-  this.setAttribute('data-placeholder', value)
+  this.setAttribute('data-placeholder', value);
+  setValue(this.value);
 }
 
-inputDate.addEventListener('input', getValue);
-inputTime.addEventListener('input', getValue);
+function setValue(value) {
+  const pickedDate = new Date(value);
+  const day = new Intl.DateTimeFormat('en-US', { weekday: 'long'}).format(pickedDate)
+  const date = pickedDate.getDate();
+  const month = new Intl.DateTimeFormat('en-US', { month: 'long'}).format(pickedDate);
+  TICKET_DATE.innerText = `${day}, ${month} ${date}`;
+}
+
+function checkInputs() {
+  if (INPUT_EMAIL.value === '') {
+    showError(INPUT_EMAIL, ERROR_TEXT_EMAIL);
+  }
+}
+
+function showError(input, message) {
+  input.nextElementSibling.innerHTML = message;
+}
+
+const checkNameParameters = () => {
+  const regName = /^[а-яА-Яёa-zA-Z\s]+$/;
+
+  if ((INPUT_NAME.value.length > 15 || INPUT_NAME.value.length < 3) || !(regName.test(INPUT_NAME.value))) return false;
+  
+  return true;
+};
+
+const checkEmailParameters= () => {
+  const regEmail = /^[a-z0-9_-]{3,15}@[a-z]{4,}\.[a-z]{2,}$/i;
+  if (!(regEmail.test(INPUT_EMAIL.value))) return false;
+  
+  return true;
+};
+
+
+const checkPhoneParameters = (phone) => {
+  let totalDigitCount = 0;
+  let currentGroupDigitCount = 0;
+  let isHyphen = false;
+  let isSpace = false;
+  const hyphen = '-';
+  const space = ' ';
+  let phoneNumber = phone;
+
+  function isNumber(char) {
+    return !Number.isNaN(+char);
+  }
+
+  function isAcceptableChar(char) {
+    return isNumber(char) || char === hyphen || char === space;
+  }
+
+  for (let char of phone) {
+    if (!isAcceptableChar(char)) {
+      return false;
+    }
+    if (char === space) {
+      if (currentGroupDigitCount < 2 && !isSpace) {
+        return false;
+      }
+      else {
+        isSpace = true;
+        currentGroupDigitCount = 0;
+      }
+    }
+    if (char === hyphen) {
+      if ((currentGroupDigitCount < 2) && !isHyphen) {
+        return false;
+      }
+      else {
+        isSpace = true;
+        currentGroupDigitCount = 0;
+      }
+    }
+    if (char !== space && isNumber(char)) {
+      totalDigitCount++;
+      if (currentGroupDigitCount < 3) {
+        currentGroupDigitCount++;
+      }
+    }
+    if (!isNumber(char) && totalDigitCount > 10) {
+       return false;
+    }
+    if ((char === hyphen || char === space) && totalDigitCount >= 10) {
+      return false;
+    }
+    if (totalDigitCount === 0 || totalDigitCount > 10) {
+      return false;
+    }
+  }
+
+  if (phone.split(space).some((el, index, array) => (array.length > 1 && el.length > 3))) {
+    return false;
+  }
+
+  if (phone.split(hyphen).some((el, index, array) => (array.length > 1 && el.length > 3))) {
+    return false;
+  }
+
+  if (currentGroupDigitCount < 2) return false;
+
+
+  return true;
+};
+
+bookBtn.addEventListener('click', showBookingForm);
+closeBookingBtn.addEventListener('click', closeBookingForm);
+
+paymentBtn.addEventListener('click', addRipple);
+
+INPUT_DATE.addEventListener('input', getValue);
+INPUT_TIME.addEventListener('input', getValue);
+
+INPUT_DATE.min = MIN_DATE;
+
+
+FORM.addEventListener('submit', (event) => {
+  event.preventDefault();
+  checkInputs();
+});
+
+INPUT_NAME.addEventListener('input', () => {
+  const isValid = checkNameParameters();
+
+  if (!isValid) {
+    INPUT_NAME.parentElement.classList.remove('valid');
+    INPUT_NAME.parentElement.classList.add('error');
+    showError(INPUT_NAME, ERROR_TEXT_NAME);
+  }
+  else {
+    INPUT_NAME.parentElement.classList.remove('error');
+    INPUT_NAME.parentElement.classList.add('valid');
+    showError(INPUT_NAME, '');
+  }
+});
+
+INPUT_EMAIL.addEventListener('input', () => {
+  const isValid = checkEmailParameters();
+  if (!isValid) {
+    INPUT_EMAIL.parentElement.classList.remove('valid');
+    INPUT_EMAIL.parentElement.classList.add('error');
+    showError(INPUT_EMAIL, ERROR_TEXT_EMAIL);
+  }
+  else {
+    INPUT_EMAIL.parentElement.classList.remove('error');
+    INPUT_EMAIL.parentElement.classList.add('valid');
+    showError(INPUT_EMAIL, '');
+  }
+});
+
+INPUT_PHONE.addEventListener('input', (event) => {
+  const isValid = checkPhoneParameters(INPUT_PHONE.value);
+  if (!isValid) {
+    INPUT_PHONE.parentElement.classList.remove('valid');
+    INPUT_PHONE.parentElement.classList.add('error');
+    showError(INPUT_PHONE, ERROR_TEXT_PHONE);
+  }
+  else {
+    INPUT_PHONE.parentElement.classList.remove('error');
+    INPUT_PHONE.parentElement.classList.add('valid');
+    showError(INPUT_PHONE, '');
+  }
+});
