44a1c1328e30ba460b30b682c466501db565132c
Marie Serykh <marie.serykh@gmail.com>
2021-10-10 09:34:51 +0200

    feat: implement ticket price calculator in section ticket

---
+++ museum-dom/ticket-calculator.js
@@ -0,0 +1,91 @@
+const BASIC_PRICES = {
+  'permanent_exhibition': 20,
+  'temporary_exhibition': 25,
+  'combined_admission': 40,
+};
+
+const SENIOR_DISCOUNT = 0.5;
+
+const SENIOR_PRICES = { 
+  'permanent_exhibition': (1 - SENIOR_DISCOUNT) * BASIC_PRICES['permanent_exhibition'],
+  'temporary_exhibition': (1 - SENIOR_DISCOUNT) * BASIC_PRICES['temporary_exhibition'],
+  'combined_admission': (1 - SENIOR_DISCOUNT) * BASIC_PRICES['combined_admission'],
+};
+
+const ticketTypePermanentOverview = document.querySelector('.js-overview-ticket-type-permanent');
+const ticketTypeTemporaryOverview = document.querySelector('.js-overview-ticket-type-temporary');
+const ticketTypeCombinedOverview = document.querySelector('.js-overview-ticket-type-combined');
+
+const ticketQtyContainer = document.querySelector('.js-overview-ticket-qty-container');
+
+const ticketBasicAmountInputOverview = document.querySelector('.js-overview-basic-ticket-qty-input');
+const ticketSeniorAmountInputOverview = document.querySelector('.js-overview-senior-ticket-qty-input');
+const ticketAmountInputsOverwview = document.querySelectorAll('.js-overview-ticket-qty-input');
+let ticketBasicAmountOverview = ticketBasicAmountInputOverview.value;
+let ticketSeniorAmountOverview = ticketSeniorAmountInputOverview.value;
+
+const addTicketBtnsOverview = document.querySelectorAll('.js-overview-add-ticket');
+const removeTicketBtnsOverview = document.querySelectorAll('.js-overview-remove-ticket');
+
+const ticketTypes = document.querySelectorAll('.js-overview-ticket-type');
+
+const totalAmountOverview = document.querySelector('.js-overview-total');
+
+function findContainer(btn) {
+  let container = btn.parentNode;
+  while (container.className === 'js-overview-ticket-qty-container') {
+    container = container.parentNode;
+  }
+  return container;
+}
+
+function findInput(container) {
+  for (let input of container.childNodes) {
+    if (input === ticketBasicAmountInputOverview || input === ticketSeniorAmountInputOverview) {
+      return input;
+    }
+  }
+}
+
+function addTicketQty(input) {
+  return input.value = (input.value < 20) ? Number(input.value) + 1 : 20;
+}
+
+function removeTicketQty(input) {
+  return input.value = (input.value > 0) ? Number(input.value) - 1 : 0;
+}
+
+
+function recalculateAmount(event) {
+  const container = findContainer(event.currentTarget);
+  const input  = findInput(container);
+  let inputValue = 0;
+  if (event.currentTarget.classList.contains('js-overview-add-ticket')) {
+    inputValue = addTicketQty(input);
+  }
+  if (event.currentTarget.classList.contains('js-overview-remove-ticket')) {
+    inputValue = removeTicketQty(input);
+  }
+  recalculateTotal();
+  return inputValue;
+}
+
+let pickedTicketType = ([...ticketTypes].filter(ticketType => ticketType.checked)[0] || {}).id;
+
+function getTicketType() {
+  pickedTicketType = ([...ticketTypes].filter(ticketType => ticketType.checked)[0] || {}).id;
+  recalculateTotal();
+}
+
+function recalculateTotal() {
+  qtyBasic = +ticketAmountInputsOverwview[0].value;
+  qtySenior = +ticketAmountInputsOverwview[1].value;
+  ticketType = pickedTicketType;
+  totalAmountOverview.textContent = BASIC_PRICES[ticketType] * qtyBasic + SENIOR_PRICES[ticketType] * qtySenior;
+}
+
+recalculateTotal();
+
+addTicketBtnsOverview.forEach(btn => btn.addEventListener('click', recalculateAmount));
+removeTicketBtnsOverview.forEach(btn => btn.addEventListener('click', recalculateAmount));
+ticketTypes.forEach(ticketType => ticketType.addEventListener('change', getTicketType));
