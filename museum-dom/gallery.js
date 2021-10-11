0e6d1cc693d09c3d7dfb6ea0d523ed4723c979f5
Marie Serykh <marie.serykh@gmail.com>
2021-10-11 21:01:22 +0200

    feat: implement gallery animation

--- museum-dom/gallery.js
+++ museum-dom/gallery.js
@@ -1,6 +1,13 @@
+const imgInnerContainer = document.querySelector('.gallery__inner-container');
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 1; i--) {
+    let j = Math.floor(Math.random() * (i)); 
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 function mixImages() {
-  const imgInnerContainer = document.querySelector('.gallery__inner-container');
-  
   let imgPathArr= [];
   
   let i = 0;
@@ -9,16 +16,41 @@ function mixImages() {
     i++;
   }
   
-  function shuffle(array) {
-    for (let i = array.length - 1; i > 1; i--) {
-      let j = Math.floor(Math.random() * (i)); 
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-}
-  
   shuffle(imgPathArr);
   imgInnerContainer.innerHTML = '';
   imgPathArr.map(image => imgInnerContainer.innerHTML += `<img class="gallery__item" src=${image} alt="Artwork of Louvre collection">`);
 }
 
 mixImages();
+
+const scroll = window.requestAnimationFrame || 
+               function(callback) {
+                 window.setTimeout(callback, 1000 / 60);
+               };
+const galleryItems = document.querySelectorAll('.gallery__item');
+
+
+function isItemInViewport(item) {
+  const rect = item.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+function checkVisibility() {
+  Array.prototype.forEach.call(galleryItems, function(item) {
+    if (isItemInViewport(item)) {
+      item.classList.add('show');
+    } else {
+      item.classList.remove('show');
+    }
+  });
+
+  scroll(checkVisibility);
+}
+
+checkVisibility();
