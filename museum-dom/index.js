0a637f4ad2f691eaa493127e1b8ffd8e0cf6c89a
Marie Serykh <marie.serykh@gmail.com>
2021-10-12 11:20:17 +0200

    feat: implement button to go up

--- museum-dom/index.js
+++ museum-dom/index.js
@@ -1,5 +1,31 @@
 const progressVideo = document.querySelector('.controls__progress-bar');
+const scrollToTopButton = document.getElementById("scrollToTop");
 
+const handleScroll = () => {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopButton.style.display = "block";
+  } 
+  else {
+    scrollToTopButton.style.display = "none";
+  }
+};
+
+const scrollToTop = () => {
+  const distanceFromTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (distanceFromTop > 0) {
+    window.scrollTo(0, 0);
+  }
+};
+
+document.addEventListener("scroll", handleScroll);
+scrollToTopButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  scrollToTop();
+});
+
+
+/*
 progressVideo.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, var(--color-accent2) 0%, var(--color-accent2) ${value}%, var(--color-player2) ${value}%)`
@@ -10,7 +36,7 @@ const volumeChange = document.querySelector('.controls__volume-slider');
 volumeChange.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, var(--color-accent2) 0%, var(--color-accent2) ${value}%, var(--color-player2) ${value}%)`
-})
+})*/
 
 const toggleBtn = document.querySelector('.menu-toggle');
 const mobileMenu = document.querySelector('.mobile-navigation');
