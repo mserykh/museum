e18b7a3ce8a671916590fc2f6bb4905480c81e64
Marie Serykh <marie.serykh@gmail.com>
2021-10-12 22:52:59 +0200

    refactor: update slider in welcome section

--- museum-dom/swiper.js
+++ museum-dom/swiper.js
@@ -1,20 +1,21 @@
 const currentSlideIndex = document.querySelector('.slider__current-slide');
 const lastSlideIndex = document.querySelector('.slider__last-slide');
 
-new Swiper('.welcome__slider', {
+const swiper = new Swiper('.welcome__slider', {
   slidesPerView: 1,
   loop: true,
   paginationClickable: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.slider__dots',
     clickable: true,
   },
   navigation: {
 		nextEl: ".slider__arrow--forward",
 		prevEl: ".slider__arrow--back",
 	},
-  onAny(activeIndexChange, slides) {
-    currentSlideIndex.innerHTML = `${slides.realIndex + 1}`;
-    lastSlideIndex.innerHTML = `${slides.slides.length - 2}`;
-  },
+});
+
+swiper.on('activeIndexChange', function () {
+  currentSlideIndex.innerHTML = `0${swiper.realIndex + 1}`;
+  lastSlideIndex.innerHTML = `0${swiper.slides.length - 2}`;
 });
