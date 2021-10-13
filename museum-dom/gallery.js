bf7ad0ae51db1796448f9d86dcaadfaa2aafd958
Marie Serykh <marie.serykh@gmail.com>
2021-10-13 23:16:28 +0200

    fix: remove gallery animation on scrolling up

--- museum-dom/gallery.js
+++ museum-dom/gallery.js
@@ -35,7 +35,7 @@ function isItemInViewport(item) {
   return (
     (rect.top <= 0 && rect.bottom >= 0)
     ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    (rect.top <= (window.innerHeight || document.documentElement.clientHeight))
     ||
     (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
